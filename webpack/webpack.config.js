0ca9bb7f9f67a3f0d1dbb814373293043bad56a3
anmol-16 <anmol.aggarwal@comprotechnologies.com>
2023-03-24 09:24:10 +0530

    removed webpack

--- webpack/webpack.config.js
+++
@@ -1,5 +0,0 @@
-const { merge } = require('webpack-merge');
-const backendConfig = require('./webpack/webpack.backend');
-const frontendConfig = require('./webpack/webpack.frontend');
-
-module.exports = merge(backendConfig, frontendConfig);

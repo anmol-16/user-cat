05e2d6e54cff28ecffbf09fddc8ead14fa31a977
anmol-16 <anmol.aggarwal@comprotechnologies.com>
2023-03-24 09:58:26 +0530

    added new features

--- user-cat-back/services/s3/services/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    s3UploadBucket:'lexev-main-s3',
-    s3Creds:{
-        bucketRegion:process.env.AWS_BUCKET_REGION,
-        accessKey:process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY
-    }
-    
-}
